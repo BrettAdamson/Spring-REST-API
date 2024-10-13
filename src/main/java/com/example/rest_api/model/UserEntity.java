fb4601f3e4ce8c9d9ef1507def6845328673d5e4
BrettAdamson <brettmadamson@gmail.com>
2024-10-12 21:41:53 -0500

    setup basic project scaffold and user entity

---
+++ src/main/java/com/example/rest_api/model/UserEntity.java
@@ -0,0 +1,26 @@
+package com.example.rest_api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserEntity {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    private String phoneNumber;
+}
---
+++ src/main/java/com/example/rest_api/service/UserService.java
@@ -0,0 +1,7 @@
+package com.example.rest_api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
