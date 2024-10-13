477ce6b51dc81694e2b75d717d56838425e10fcc
BrettAdamson <brettmadamson@gmail.com>
2024-10-12 22:02:29 -0500

    controller method signatures done

--- src/main/java/com/example/rest_api/controller/UserController.java
+++ src/main/java/com/example/rest_api/controller/UserController.java
@@ -1,10 +1,42 @@
 package com.example.rest_api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.rest_api.model.UserEntity;
+import com.example.rest_api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserEntity>> getAllUsers() {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserEntity> getUserById(@PathVariable Integer id) {
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity createUser(@RequestBody UserEntity userEntity) {
+        return null;
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<UserEntity> updateUser(@PathVariable Integer id, @RequestBody UserEntity userEntityDetails){
+        return null;
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUsers(@PathVariable Integer id){
+        return null;
+    }
+
 
 }
