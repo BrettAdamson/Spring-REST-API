ad7cb59a86229b2727c2382adce32dfc2f35497e
BrettAdamson <brettmadamson@gmail.com>
2024-10-13 19:09:47 -0500

    added regex for email and phone number matching

--- src/main/java/com/example/rest_api/service/UserService.java
+++ src/main/java/com/example/rest_api/service/UserService.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.regex.*;
 
 import java.util.List;
 
@@ -19,6 +22,8 @@ public class UserService {
     private UserRepository userRepository;
 
     public ResponseEntity createUser(UserEntity newUser){
+
+        validateUserInputs(newUser);
         UserEntity createdUser = userRepository.save(newUser);
         return ResponseEntity.ok(createdUser);
     }
@@ -41,6 +46,7 @@ public class UserService {
 
 
         UserEntity toBeUpdated = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User does not exist with id : "+ id));
+        validateUserInputs(userEntityDetails);
 
         toBeUpdated.setFirstName(userEntityDetails.getFirstName());
         toBeUpdated.setLastName(userEntityDetails.getLastName());
@@ -59,6 +65,19 @@ public class UserService {
         UserEntity toBeDeletedUser = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User does not exist with id : "+ id));
         userRepository.delete(toBeDeletedUser);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    private void validateUserInputs(UserEntity userEntity){
+        //Regex's from https://regex101.com
+        Pattern phonePattern = Pattern.compile("^(\\(\\+[0-9]{2}\\))?([0-9]{3}-?)?([0-9]{3})\\-?([0-9]{4})(\\/[0-9]{4})?$");
+        Matcher phoneMatcher = phonePattern.matcher(userEntity.getPhoneNumber());
+        Pattern emailPattern = Pattern.compile("^((?!\\.)[\\w\\-_.]*[^.])(@\\w+)(\\.\\w+(\\.\\w+)?[^.\\W])$");
+        Matcher emailMatcher = emailPattern.matcher(userEntity.getEmail());
+        if(!phoneMatcher.matches()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Phone Number");
+        }else if(!emailMatcher.matches()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Email Address");
+        }
     }
 
 }
